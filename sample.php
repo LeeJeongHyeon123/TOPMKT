15b3290bc8866f354ca011be4f092c70ee8ae6c4
JH Lee <2jeonghyeon@naver.com>
2025-05-06 19:25:58 +0900

    sample.php 파일 업데이트 - 새 함수 추가 및 내용 수정

--- sample.php
+++ sample.php
@@ -4,6 +4,7 @@
  * 
  * @author JH Lee
  * @date <?php echo date('Y-m-d'); ?>
+ * @version 1.1
  */
 
 // 간단한 함수 예제
@@ -11,11 +12,17 @@ function sayHello($name) {
     return "안녕하세요, {$name}님!";
 }
 
+// 두 번째 함수 추가
+function getCurrentDateTime() {
+    return date('Y-m-d H:i:s');
+}
+
 // 현재 시간 출력
-$current_time = date('Y-m-d H:i:s');
+$current_time = getCurrentDateTime();
 echo "현재 시간: {$current_time}";
 
 // 테스트 메시지
-echo "<h1>Git 테스트 파일입니다</h1>";
+echo "<h1>Git 테스트 파일입니다 (업데이트됨)</h1>";
 echo "<p>" . sayHello("사용자") . "</p>";
+echo "<p>이 파일은 Git 커밋 테스트를 위해 수정되었습니다.</p>";
 ?> 
